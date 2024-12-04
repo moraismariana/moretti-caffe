04efe3515d60cc79f14724ebd78db1e94d74473b
moraismariana <moraismariana200@gmail.com>
2024-12-04 15:00:15 -0300

    Cardapio GET API

---
+++ admin/assets/js/script.js
@@ -0,0 +1,5 @@
+import initListSizeStyle from "./modules/list-size-style.js";
+import initModal from "./modules/modal.js";
+
+initListSizeStyle();
+initModal();
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
 import initMenuMobile from "./modules/menu-mobile.js";
 import initLogoMenu from "./modules/logo-menu.js";
 import initScrollEffect from "./modules/scroll-effect.js";
+import initGetAPI from "./modules/get-api.js";
 
 initMenuMobile();
 initLogoMenu();
 initScrollEffect();
+initGetAPI();
