c9ecd29e835080f39a2cf596df8427372311b88c
moraismariana <moraismariana200@gmail.com>
2024-12-27 20:15:43 -0300

    Opacidade ao carregar API CMS

--- static/js/modules/estilizacao/scroll-effect.js
+++ static/js/modules/estilizacao/scroll-effect.js
@@ -2,29 +2,31 @@ export default function initScrollEffect() {
   // EFEITO DE SCROLL
   const scrollElement = document.querySelectorAll("[data-scroll-element]");
 
-  scrollElement.forEach((element) => {
-    if (isInView(element)) {
-      element.classList.add("transition-element--visible");
-    }
-  });
-
-  document.addEventListener("scroll", function () {
+  if (scrollElement.length > 0) {
     scrollElement.forEach((element) => {
       if (isInView(element)) {
         element.classList.add("transition-element--visible");
-      } else {
-        element.classList.remove("transition-element--visible");
       }
     });
-  });
 
-  function isInView(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.bottom > 0 &&
-      rect.top <
-        (window.innerHeight - 120 ||
-          document.documentElement.clientHeight - 120)
-    );
+    document.addEventListener("scroll", function () {
+      scrollElement.forEach((element) => {
+        if (isInView(element)) {
+          element.classList.add("transition-element--visible");
+        } else {
+          element.classList.remove("transition-element--visible");
+        }
+      });
+    });
+
+    function isInView(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.bottom > 0 &&
+        rect.top <
+          (window.innerHeight - 120 ||
+            document.documentElement.clientHeight - 120)
+      );
+    }
   }
 }
